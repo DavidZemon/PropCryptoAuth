bb079eb9083d58d3f004d851ed1422d37ffbaded
David Zemon <david@zemon.name>
2019-01-22 19:26:54 -0600

    Add factory default configuration and some macros to help change it

--- demo/cryptoauth_demo.cpp
+++ demo/cryptoauth_demo.cpp
@@ -20,9 +20,104 @@
  */
 
 #include <PropWare/hmi/output/printer.h>
+#include <PropWare/memory/blockstorage.h>
 #include <atca_basic.h>
 
 using PropWare::Printer;
+using PropWare::Utility;
+using PropWare::BlockStorage;
+
+/**
+ * @brief Combine slot configuration values into a two-byte slot configuration
+ *
+ * @param[in] writeConfig
+ * @param[in] writeKey
+ * @param[in] isSecret          Instance of IsSecret
+ * @param[in] encryptRead       Instance of IsEncrypted
+ * @param[in] limitedUse        Instance of SlotMultiUseLimitation
+ * @param[in] noMac             Instance of SlotMacAllowed
+ * @param[in] readKey           Either a slot ID for the key to be used for encrypting reads of this slot or
+ *                              combination of `(EcdhOutputTarget | EcdhPermission | MessageSignatureEnable |
+ *                              ExternalSignatureEnable)`
+ */
+#define SLOT_CONFIGURATION(writeConfig, writeKey, isSecret, encryptRead, limitedUse, noMac, readKey) ( \
+    ((writeConfig & 0x0f) << 12) | \
+    ((writeKey & 0x0f) << 8) | \
+    isSecret | \
+    encryptRead | \
+    limitedUse | \
+    noMac | \
+    (readKey & 0x0f)
+)
+
+typedef enum {
+    SLOT_IS_PUBLIC,
+    SLOT_IS_SECRET = 0x80
+} IsSecret;
+
+typedef enum {
+    SLOT_IS_PLAIN_TEXT,
+    SLOT_IS_ENCRYPTED = 0x40
+} IsEncrypted;
+
+typedef enum {
+    SLOT_USE_UNLIMITED,
+    SLOT_USE_LIMITED = 0x20
+} SlotMultiUseLimitation;
+
+typedef enum {
+    NO_USAGE_RESTRICTIONS,
+    VERFICATION_ONLY = 0x10
+} SlotMacAllowed;
+
+typedef enum {
+    ECDH_WRITE_NORMAL,
+    ECDH_WRITE_TO_SLOT = 0x08
+} EcdhOutputTarget;
+
+typedef enum {
+    ECDH_OP_FORBIDDEN,
+    ECDH_OP_PERMITTED = 0x04
+} EcdhPermission;
+
+typedef enum {
+    DISABLE_MSG_SIGNATURES,
+    ENABLE_MSG_SIGNATURES = 0x02
+} MessageSignatureEnable;
+
+typedef enum {
+    DISABLE_EXT_SIGNATURES,
+    ENABLE_EXT_SIGNATURES = 0x01
+} ExternalSignatureEnable;
+
+/**
+ * @brief Complete configuration data for the ATECC508A
+ *
+ * Data can only be written to devices in 4- or 32-byte chunks, so it's not worth trying to write the configuration
+ * in a byte-by-byte manner. For this reason, we'll just configure the entire configuration zone at once and hardcode
+ * a big block.
+ *
+ * 0xFF will be used for the read-only bytes serial number and revision information
+ */
+// @formatter:off
+static const uint8_t CONFIG_ZONE_DATA_ATECC508A[] = {
+//            0     1     2     3     4     5     6     7       8     9     A     B     C     D     E     F
+/* 0x00 */ 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,   0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
+/* 0x10 */ 0xC0, 0x00, 0x55, 0x00,
+
+// Special slot configuration (bytes 20-51)
+/* 0x10 */                         0x83, 0x20, 0x87, 0x20,   0x8F, 0x20, 0xC4, 0x8F, 0x8F, 0x8F, 0x8F, 0x8F,
+/* 0x20 */ 0x9F, 0x8F, 0xAF, 0x8F, 0x00, 0x00, 0x00, 0x00,   0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+/* 0x30 */ 0x00, 0x00, 0xAF, 0x8F,
+
+// Normal configuration again
+/* 0x30 */                         0xFF, 0xFF, 0xFF, 0xFF,   0x00, 0x00, 0x00, 0x00, 0xFF, 0xFF, 0xFF, 0xFF,
+/* 0x40 */ 0x00, 0x00, 0x00, 0x00, 0xFF, 0xFF, 0xFF, 0xFF,   0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
+/* 0x50 */ 0xFF, 0xFF, 0xFF, 0xFF, 0x00, 0x00, 0x55, 0x55,   0xFF, 0xFF, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+/* 0x60 */ 0x33, 0x00, 0x33, 0x00, 0x33, 0x00, 0x1C, 0x00,   0x1C, 0x00, 0x1C, 0x00, 0x1C, 0x00, 0x1C, 0x00,
+/* 0x70 */ 0x3C, 0x00, 0x3C, 0x00, 0x3C, 0x00, 0x3C, 0x00,   0x3C, 0x00, 0x3C, 0x00, 0x3C, 0x00, 0x1C, 0x00
+};
+// @formatter:on
 
 static const Printer::Format HEX_FMT(2, '0', 16);
 
@@ -62,15 +157,17 @@ Printer &operator<< (Printer &printer, const ATCAIfaceCfg &cfg) {
 }
 
 void print_block (const Printer &printer, const uint8_t *const data, const size_t length) {
-    for (int i = length; i; --i)
-        printer.put_int(data[i], 16, 2, '0');
+    char buffer[256];
+    auto bufferSize = Utility::size_of_array(buffer);
+    atcab_bin2hex(data, length, buffer, &bufferSize);
+    printer << buffer;
 }
 
 class CryptoDevice {
     public:
         static const uint8_t        SHA256_DEFAULT_ADDRESS = 0xC0;
-        static const ATCADeviceType DEFAULT_DEVICE_TYPE    = ATECC608A;
-        static const uint32_t       DEFAULT_BAUD           = 400000;
+        static const ATCADeviceType DEFAULT_DEVICE_TYPE    = ATECC508A;
+        static const uint32_t       DEFAULT_BAUD           = 1000000;
         static const uint8_t        DEFAULT_BUS            = 0;
         static const uint16_t       DEFAULT_WAKE_DELAY     = 800;
         static const uint8_t        DEFAULT_RX_RETRIES     = 3;
@@ -113,7 +210,7 @@ class CryptoDevice {
             PropWare::ErrorCode err;
             uint8_t             serialNumber[ATCA_SERIAL_NUM_SIZE];
             check_errors(atcab_read_serial_number(serialNumber));
-            printer << "Serial number: 0x";
+            printer << "Serial number: ";
             print_block(printer, serialNumber, ATCA_SERIAL_NUM_SIZE);
             return 0;
         }
@@ -143,7 +240,21 @@ PropWare::ErrorCode run (CryptoDevice &cryptoDevice) {
     check_errors(cryptoDevice.print_serial(pwOut));
     pwOut << '\n';
 
-    pwOut << "Generating new public/private key: 0x";
+    pwOut << "Initial configuration zone:\n";
+    uint8_t    configData[128];
+    const auto configDataSize = Utility::size_of_array(configData);
+    check_errors(atcab_read_config_zone(configData));
+    BlockStorage::print_block(pwOut, configData, configDataSize);
+
+    check_errors(atcab_write_config_zone(CONFIG_ZONE_DATA_ATECC508A));
+
+    pwOut << "Final configuration zone:\n";
+    uint8_t    configData[128];
+    const auto configDataSize = Utility::size_of_array(configData);
+    check_errors(atcab_read_config_zone(configData));
+    BlockStorage::print_block(pwOut, configData, configDataSize);
+
+    pwOut << "Generating new public/private key: ";
     check_errors(cryptoDevice.generate_key(1, &pwOut));
     pwOut << '\n';
 
