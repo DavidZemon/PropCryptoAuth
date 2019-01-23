ae9be242f6714a1cf86693c9b0af68785c95aca4
David Zemon <david@zemon.name>
2019-01-23 09:24:00 -0600

    Refactor a bit to make it easier to read configurations

--- demo/cryptoauth_demo.cpp
+++ demo/cryptoauth_demo.cpp
@@ -27,8 +27,6 @@ using PropWare::Printer;
 using PropWare::Utility;
 using PropWare::BlockStorage;
 
-#define BYTE_SWAP(x) (((x & 0xff00) >> 8) | (x & 0xff))
-
 /**
  * @brief Combine slot configuration values into a two-byte slot configuration
  *
@@ -42,14 +40,17 @@ using PropWare::BlockStorage;
  *                              combination of `(EcdhOutputTarget | EcdhPermission | MessageSignatureEnable |
  *                              ExternalSignatureEnable)`
  */
-#define SLOT_CONFIGURATION(writeConfig, writeKey, isSecret, encryptRead, limitedUse, noMac, readKey) BYTE_SWAP( \
-    ((writeConfig & 0x0f) << 12) | \
-    ((writeKey & 0x0f) << 8) | \
+#define SLOT_CONFIGURATION(writeConfig, writeKey, isSecret, encryptRead, limitedUse, noMac, readKey) ( \
+    /* High order byte */ \
+    (((writeConfig) & 0x0f) << 12) | \
+    (((writeKey) & 0x0f) << 4) | \
+    \
+    /* Low order byte */ \
     isSecret | \
     encryptRead | \
     limitedUse | \
     noMac | \
-    (readKey & 0x0f) \
+    (readKey & 0x0f00) \
 )
 
 typedef enum {
@@ -92,6 +93,18 @@ typedef enum {
     ENABLE_EXT_SIGNATURES = 0x01
 } ExternalSignatureEnable;
 
+#define SPLIT(x) static_cast<uint8_t>(x), static_cast<uint8_t>(x >> 8)
+
+#define SWAP(x) static_cast<uint16_t>((x << 8) | (x & 0xff))
+
+#define PUBLIC_KEY SLOT_CONFIGURATION(0b0010, 0, SLOT_IS_SECRET, SLOT_IS_PLAIN_TEXT, SLOT_USE_UNLIMITED, \
+                                      NO_USAGE_RESTRICTIONS, 1)
+
+/**
+ * @brief Swap the bytes and split them into two distinct parameters that can be added as literals in a byte-array
+ */
+#define _PUB_KEY SPLIT(SWAP(PUBLIC_KEY))
+
 /**
  * @brief Complete configuration data for the ATECC508A
  *
@@ -104,6 +117,19 @@ typedef enum {
  * @note This method is NOT ideal because it prevents the program from computing a valid CRC for the configuration
  *       data block. It would be better to read the configuration zone at runtime, then set only the bits/bytes that
  *       matter for the application, calculate a CRC, and then write the data back with the checksum included.
+ *
+ * Before messing with your device, do a read of your configuration zone data and save it off. This is the results of my
+ * read, prior to any writes:
+ *
+ *          0  1  2  3  4  5  6  7    8  9  A  B  C  D  E  F
+ * 0x0000: 01 23 62 53 00 00 50 00 - 55 DA F7 A0 EE C0 49 00 .#bS..P.U.....I.
+ * 0x0010: C0 00 55 00 83 20 87 20 - 8F 20 C4 8F 8F 8F 8F 8F ..U.. . . ......
+ * 0x0020: 9F 8F AF 8F 00 00 00 00 - 00 00 00 00 00 00 00 00 ................
+ * 0x0030: 00 00 AF 8F FF FF FF FF - 00 00 00 00 FF FF FF FF ................
+ * 0x0040: 00 00 00 00 FF FF FF FF - FF FF FF FF FF FF FF FF ................
+ * 0x0050: FF FF FF FF 00 00 55 55 - FF FF 00 00 00 00 00 00 ......UU........
+ * 0x0060: 33 00 33 00 33 00 1C 00 - 1C 00 1C 00 1C 00 1C 00 3.3.3...........
+ * 0x0070: 3C 00 3C 00 3C 00 3C 00 - 3C 00 3C 00 3C 00 1C 00 <.<.<.<.<.<.<...
  */
 // @formatter:off
 static const uint8_t CONFIG_ZONE_DATA_ATECC508A[] = {
@@ -112,7 +138,7 @@ static const uint8_t CONFIG_ZONE_DATA_ATECC508A[] = {
 /* 0x10 */ 0xC0, 0x00, 0x55, 0x00,
 
 // Special slot configuration (bytes 20-51)
-/* 0x10 */                         DEFLT_SLOT, 0x87, 0x20,   0x8F, 0x20, 0xC4, 0x8F, 0x8F, 0x8F, 0x8F, 0x8F,
+/* 0x10 */                         _PUB_KEY,   0x87, 0x20,   0x8F, 0x20, 0xC4, 0x8F, 0x8F, 0x8F, 0x8F, 0x8F,
 /* 0x20 */ 0x9F, 0x8F, 0xAF, 0x8F, 0x00, 0x00, 0x00, 0x00,   0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
 /* 0x30 */ 0x00, 0x00, 0xAF, 0x8F,
 
@@ -120,6 +146,8 @@ static const uint8_t CONFIG_ZONE_DATA_ATECC508A[] = {
 /* 0x30 */                         0xFF, 0xFF, 0xFF, 0xFF,   0x00, 0x00, 0x00, 0x00, 0xFF, 0xFF, 0xFF, 0xFF,
 /* 0x40 */ 0x00, 0x00, 0x00, 0x00, 0xFF, 0xFF, 0xFF, 0xFF,   0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
 /* 0x50 */ 0xFF, 0xFF, 0xFF, 0xFF, 0x00, 0x00, 0x55, 0x55,   0xFF, 0xFF, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+
+// Special key configuration (bytes 96-127)
 /* 0x60 */ 0x33, 0x00, 0x33, 0x00, 0x33, 0x00, 0x1C, 0x00,   0x1C, 0x00, 0x1C, 0x00, 0x1C, 0x00, 0x1C, 0x00,
 /* 0x70 */ 0x3C, 0x00, 0x3C, 0x00, 0x3C, 0x00, 0x3C, 0x00,   0x3C, 0x00, 0x3C, 0x00, 0x3C, 0x00, 0x1C, 0x00
 };
@@ -252,17 +280,19 @@ PropWare::ErrorCode run (CryptoDevice &cryptoDevice) {
     check_errors(atcab_read_config_zone(configData));
     BlockStorage::print_block(pwOut, configData, configDataSize);
 
-    check_errors(atcab_write_config_zone(CONFIG_ZONE_DATA_ATECC508A));
-
-    pwOut << "Final configuration zone:\n";
-    uint8_t    configData[128];
-    const auto configDataSize = Utility::size_of_array(configData);
-    check_errors(atcab_read_config_zone(configData));
-    BlockStorage::print_block(pwOut, configData, configDataSize);
-
-    pwOut << "Generating new public/private key: ";
-    check_errors(cryptoDevice.generate_key(1, &pwOut));
+    pwOut << "Slot config: 0x";
+    pwOut.put_int(PUBLIC_KEY, 16, 4, '0');
     pwOut << '\n';
+
+    //pwOut << "Writing modified configuration zone data\n";
+    //check_errors(atcab_write_config_zone(CONFIG_ZONE_DATA_ATECC508A));
+    //pwOut << "Final configuration zone:\n";
+    //check_errors(atcab_read_config_zone(configData));
+    //BlockStorage::print_block(pwOut, configData, configDataSize);
+
+    //pwOut << "Generating new public/private key: ";
+    //check_errors(cryptoDevice.generate_key(1, &pwOut));
+    //pwOut << '\n';
 
     return 0;
 }
